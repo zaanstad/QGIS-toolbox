f9eb9917aba8ab64b12706e044a2f9b4c079c210
Bart Baas <b.baas@baasgeo.com>
2015-01-12 15:47:16 +0100

    Convert lines to midpoints

---
+++ MidPoints.py
@@ -0,0 +1,23 @@
+##[Zaanstad]=group
+##input=vector
+##output=output vector
+
+from PyQt4.QtCore import *
+from qgis.core import *
+
+from processing.core.VectorWriter import VectorWriter
+
+inputLayer = processing.getObject(input)
+features = processing.features(inputLayer)
+fields = inputLayer.pendingFields().toList()
+outputLayer = VectorWriter(output, None, fields, QGis.WKBPoint, inputLayer.crs())
+
+for ft in features:
+  geom = ft.geometry()
+  meanPoint = geom.interpolate(geom.length() / 2)
+  attrs = ft.attributes()
+
+  outFeat = QgsFeature()
+  outFeat.setGeometry(meanPoint)
+  outFeat.setAttributes(attrs)
+  outputLayer.addFeature(outFeat)
